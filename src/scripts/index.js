91a860f7ecd519556452e89adf1812918cab9e74
rian <rianmadioto000@gmail.com>
2024-09-18 21:54:37 -0300

    criando a contagem regressiva

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,21 @@
-console.log('olá mundo');
+AOS.init();
+
+const dataDoEvento = new Date('Oct 12, 2024 21:00:00');
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEVento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEVento}s`;
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Este Evento ja foi finalizado.'
+    }
+},1000);
